4b9a434ae84b74c9d9a2268196deb4a08678d65a
selen <selennalt@gmail.com>
2025-02-28 21:59:58 +0300

    Create two_sum.py

---
+++ two_sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+
+        list=[]
+        
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]+nums[j]==target):
+                    list.append(i)
+                    list.append(j)
+
+
+        return list
+        
