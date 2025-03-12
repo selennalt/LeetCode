753cb30c4c1bb2705d9957ca7ba65f49bfcb659a
selen <102906951+selennalt@users.noreply.github.com>
2025-03-12 10:33:36 +0300

    Update palindrome_num.c

--- palindrome_num.c
+++ palindrome_num.c
@@ -1,10 +1,8 @@
 
 bool isPalindrome(int x) {
 
-    int a=x;
-    int b=x;
-    int i,j,k;
-    int len=0;
+    int a=x,b=x;
+    int i,j,k,len=0;
 
     if(x<0){
         return false;
@@ -19,26 +17,24 @@ bool isPalindrome(int x) {
         len++;
     }
 
-    
-
     int dizi[len];
 
     for(i=len-1;i>=0;i--){
 
         dizi[i]=b%10;
             b/=10;
-    }
+   }
+    
     for(j=0;j<len/2;j++){
 
         if(dizi[j]!=dizi[len-1-j]){
 
             return false;
-        }
+         }
 
-    }
+   } 
 
     return true;
-
 }
 
 
